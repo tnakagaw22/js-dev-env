6804d54aefe494ad8a3d08259daca9241b7b014f
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-11-08 02:04:31 -0500

    created userAPI which request '/users'

--- src/index.test.js
+++ src/index.test.js
@@ -13,7 +13,7 @@ describe('index.html', () =>{
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(erro, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal('hello world!');
+      expect(h1.innerHTML).to.equal('Users');
       done();
       window.close();
     });
