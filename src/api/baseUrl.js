9049f54e2329c9bc75dd6719e8c3611940d073c3
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-11-30 00:11:57 -0500

    set up the script to automate build

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,15 @@
 export default function getBaseUrl(){
-  const isDeveloopment = window.location.hostname === 'localhost';
-  return isDeveloopment ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+
+  /* eslint-disable no-console */
+  // to use mock API : http://localhost:3000/?useMockApi=true
+  // to use production (express) : http://localhost:3000
+}
+
+function getQueryStringParameterByName(name, url){
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(url);
+  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
