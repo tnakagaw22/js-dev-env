73ea393ecc101a145f0029eb17052a37fab48f43
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-11-30 21:07:49 -0500

    separate vendor libraries in vendor.js bundle -When you have multiple entries, the output filename will be main.js or vendor.js using entry's key name because of '[name].js'. -webpack.optimize.CommonsChunkPlugin takes entry's key as name.

---
+++ src/vendor.js
@@ -0,0 +1,9 @@
+/*
+  This file contains references to the vendor libraries used in this project.
+  This is used by webpack in the production build only.
+  Vendor libraries are bundled together separately from the projet codes so that it can be cached.
+*/
+
+/*eslint-disable no-unused-vars */
+
+import fetch from 'whatwg-fetch';
