11f1b803397b154839894cb42c17631bdc42e6f0
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-12-02 10:20:33 -0500

    added Sentry.js error logger

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -2,12 +2,20 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 import compression from 'compression';
+import Raven from'raven';
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 
+// Must configure Raven before doing anything else with it
+// DSN can be found in https://sentry.io/tomohiro-nakagawa/js-dev-env/settings/keys/
+const dsn = 'https://c74465110646402090e91ba4f99c4881:01f46686830a42fe8bc0defa0c972076@sentry.io/253615';
+Raven.config(dsn).install();
+
+// The request handler must be the first middleware on the app
+app.use(Raven.requestHandler());
 app.use(compression());
 app.use(express.static('dist'));
 
@@ -21,6 +29,18 @@ app.get('/users', function(req, res){
     {"id":2, "firstName":"Tammy", "lastName":"Norton", "email": "tammy@gmail.com"},
     {"id":3, "firstName":"Tina", "lastName":"Lee", "email": "lee@gmail.com"}
   ]);
+});
+
+
+// The error handler must be before any other error middleware
+app.use(Raven.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) { //eslint-disable-line no-unused-vars
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + '\n');
 });
 
 app.listen(port, function(err){
