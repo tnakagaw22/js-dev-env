5ca15d7a63637cf67989bacb3c152b19db23b0e4
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-12-27 00:02:24 -0500

    changed vue loader's version to avoid unexpected token export

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -21,6 +21,11 @@ module.exports = {
       inject: true
     })
   ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
